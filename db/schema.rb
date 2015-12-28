ffa057e12a36e3d7dbd645a7625eaf0af58fd665
sagnik1 <sagnikProj@outlook.com>
2015-12-29 02:35:35 +0530

    Comment feature added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151228163334) do
+ActiveRecord::Schema.define(version: 20151228200940) do
 
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at"
@@ -19,5 +19,15 @@ ActiveRecord::Schema.define(version: 20151228163334) do
     t.string   "title"
     t.text     "body"
   end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "author_name"
+    t.text     "body"
+    t.integer  "article_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id"
 
 end
